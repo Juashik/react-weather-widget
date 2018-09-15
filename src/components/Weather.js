a651f38715c7d71801ef29a6047f04b61b6f7ba9
Juashik <juashik@gmail.com>
2018-09-15 03:49:35 +0300

    Design

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,6 +1,17 @@
 import React from 'react'
+import './Weather.css'
 
-const apiUrl = 'http://api.openweathermap.org/data/2.5/weather?units=metric&lang=ru&APPID=fb0ad270a9d54fed321ffe270f87ac9f&q='
+const apiKey = 'fb0ad270a9d54fed321ffe270f87ac9f';
+const units = 'metric';
+const lang = 'ru';
+const apiUrl = `http://api.openweathermap.org/data/2.5/weather?units=${units}&lang=${lang}&appid=${apiKey}&q=`;
+
+const falloutTypes = {
+    "2": "Гроза",
+    "3": "Изморось",
+    "5": "Дождь",
+    "8": "Нет"
+}
 
 export default class Weather extends React.Component {
     constructor(props) {
@@ -15,8 +26,7 @@ export default class Weather extends React.Component {
                 humidity: "--",
                 description: "--",
                 clouds: "--",
-                main: "--",
-                rainValue: "--",
+                rainfall: "--",
                 icon: null
             }
         }
@@ -32,21 +42,19 @@ export default class Weather extends React.Component {
             .then((res) => res.json())
             .then(
                 (result) => {
-                    console.log(result);
-                    let weatherData = {
-                        city: result.name,
-                        temp: result.main.temp,
-                        humidity: result.main.humidity,
-                        description: result.weather[0].description,
-                        clouds: result.clouds.all,
-                        main: result.weather[0].main,
-                        rainValue: result.rain || result.snow,
-                        icon: result.weather[0].icon
-                    }
-                    weatherData.description = weatherData.description[0].toUpperCase() + weatherData.description.substring(1)
+                    let fallout = Math.floor(result.weather[0].id / 100);
+
                     this.setState({
                         isLoaded: true,
-                        data: weatherData
+                        data: {
+                            city: result.name,
+                            temp: Math.round(result.main.temp),
+                            humidity: result.main.humidity,
+                            description: this.capitalizeFirstLetter(result.weather[0].description),
+                            clouds: result.clouds.all,
+                            fallout: null,
+                            icon: result.weather[0].icon
+                        }
                     });
                 },
                 (error) => {
@@ -62,25 +70,38 @@ export default class Weather extends React.Component {
         this.updateWeather();
     }
 
-    componentWillUnmount() {
-
-    }
-
     render() {
         console.log(this.state);
-        //let imgUrl = "http://openweathermap.org/img/w/" + this.state.data.weather[0].icon + ".png"
+        let picture;
+        let cityName;
+
+        if (this.state.data.city === 'Moscow') {
+            cityName = 'Москва';
+        } else {
+            cityName = this.state.data.city;
+        }
+
+        if (this.state.isLoaded) {
+            picture = <img className='weather-picture' src={`http://openweathermap.org/img/w/${this.state.data.icon}.png`} alt=""/>;
+        } else {
+            picture = <div className='weather-picture' />;
+        }
+
         return (
-            <div>
-                <h3>Погода в городе {this.state.data.city}:</h3>
-                <img src={`http://openweathermap.org/img/w/${this.state.data.icon}.png`} alt=""/>
-                <ul>
-                    <li>Температура: {this.state.data.temp} {String.fromCharCode(176)}C</li>
-                    <li>Влажность: {this.state.data.humidity}%</li>
-                    <li>Описание: {this.state.data.description}</li>
-                    <li>Облачность: {this.state.data.clouds}%</li>
-                    <li>Тип осадков: {this.state.data.main}</li>
-                    <li>Количество осадков: {this.state.data.rainValue}</li>
-                </ul>
+            <div className='weather-card'>
+                <div className='weather-city'>
+                    <h3>{cityName}</h3>
+                </div>
+                {picture}
+                <div className='weather-temp'>
+                    <h1>{this.state.data.temp} {String.fromCharCode(176)}C</h1>
+                    <h4>{this.state.data.description}</h4>
+                </div>
+                <div className='weather-details'>
+                    <p>Влажность: {this.state.data.humidity}%</p>
+                    <p>Облачность: {this.state.data.clouds}%</p>
+                    <p>Осадки: {this.state.data.fallout}</p>
+                </div>
             </div>
         )
     }
